ffca556f2db91b8d58443b24e91523eb583c32f9
Shreya Shukla <shreya34shukla@gmail.com>
2026-09-20 00:51:38 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,14 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import netlify from "@netlify/vite-plugin-tanstack-start";
 
 export default defineConfig({
   plugins: [
     tanstackStart({
       server: { entry: "server" },
     }),
+    netlify(),
     react(),
     tsconfigPaths(),
     tailwindcss(),
